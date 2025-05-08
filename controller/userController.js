7a8354538c7232a330c23718152f897fdfdd4632
Ajibade Ridwan <ridwanabiola2000@gmail.com>
2025-05-08 14:21:02 +0100

    Updated Changes

--- controller/userController.js
+++ controller/userController.js
@@ -9,7 +9,7 @@ const loginUser = async (req, res) => {
 // signup user
 const signupUser = async (req, res) =>{
     const { email, password } = req.body // destructure the request body
-    
+
     try {
         const user = await User.signup(email, password) // call the signup method from the user model
         res.status(200).json({email, user}) // send the user as a response
@@ -18,8 +18,7 @@ const signupUser = async (req, res) =>{
         res.status(400).json({error: error.message}) // send the error as a response
         console.log(error.message)
     }
-
-  
+ 
 }
 
 
