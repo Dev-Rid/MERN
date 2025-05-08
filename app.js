e523656f40326df6151df7249e2615d126e085e8
Ajibade Ridwan <ridwanabiola2000@gmail.com>
2025-05-08 09:59:33 +0100

    Updated Changes

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require("dotenv").config()
 const express = require("express")
 const mongoose = require("mongoose")
 const workoutRoutes = require("./routes/workouts")
-const workoutRoutes = require("./routes/workouts")
+const userRoutes = require("./routes/user")
 
 
 // express app
@@ -19,7 +19,7 @@ app.use((req, res, next) => {
 
 // route handler
 app.use("/api/workouts", workoutRoutes)
-app.use("/api/workouts", )
+app.use("/api/user", userRoutes)
 
 
 // connect to database
