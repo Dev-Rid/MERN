bb39a5ad9a176f3182e23e86f29c5b875e490b8f
Ajibade Ridwan <ridwanabiola2000@gmail.com>
2025-05-08 21:16:23 +0100

    Updated Changes

--- model/userModel.js
+++ model/userModel.js
@@ -33,6 +33,8 @@ userSchema.statics.signup = async function (email, password) {
     if(!validator.isStrongPassword(password)){
         throw Error("Password is not strong enough")
     }
+
+    
     
     const exists = await this.findOne({ email })
     
