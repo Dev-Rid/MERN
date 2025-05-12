082a51e1221e70ab484332c4814bd153d56526d9
Ajibade Ridwan <ridwanabiola2000@gmail.com>
2025-05-12 10:53:08 +0100

    Updated Changes

--- model/userModel.js
+++ model/userModel.js
@@ -19,7 +19,7 @@ const userSchema = new Schema({
 
 
 // static signup method
-userSchema.statics.signup = async function (email, password) {
+userSchema.statics.signup = async function(email, password) {
 
     // validation
     if(!email || !password){
